7fc096e6e150adc1d16a3f8a8177b4dff7e5b870
ductlm <137411085+ductlm@users.noreply.github.com>
2023-06-23 00:08:50 +0700

    Update Test2.cpp

--- Test2.cpp
+++ Test2.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 class sinhvien {
-	private:
+	public:
 		string code;
 		string name, gender;
 		double age, maths, physics, chemis;
-	public:
 		void enter();
 		void output();
 		void edit(string masv);
 		void del(string masv);
+		sinhvien();
 		sinhvien(string masv, string ten, string gioitinh, double tuoi, double toan, double ly, double hoa);
 		void setsv(string masv, string ten, string gioitinh, double tuoi, double toan, double ly, double hoa) {
 			this->code = masv;
@@ -25,26 +25,26 @@ class sinhvien {
 
 
 class lop {
-	private:
+	public:	
 		string classname;
-	public:
 		vector <sinhvien> listsvlop;
+		vector <sinhvien>::iterator i;
 		void seeallclass();
 		void addclass();
-		void addstudentclass();
-		void studentinclass();
-		void delstudentclass();
+		void addstudentclass(string tenlop);
+		void studentinclass(string tenlop);
+		void delstudentclass(string tenlop);
+		lop();
 		lop(string classname);
 		void setclass(string tenlop) {
-			classname = tenlop;
+			this->classname = tenlop;
 			
 		}
 	
 };
 
-vector <sinhvien> listsv;
-vector <lop> listlop;
-vector <lop>:: iterator check;
+map <string, sinhvien> listsv;
+map <string, lop> listlop;
 
 
 void sinhvien::enter() {
@@ -64,7 +64,7 @@ void sinhvien::enter() {
 	cin >> ly;
 	cout << "Diem hoa: " << endl;
 	cin >> hoa;
-	listsv.push_back(sinhvien(masv, ten, gioitinh, tuoi, toan, ly, hoa));
+	setsv(masv, ten, gioitinh, tuoi, toan, ly, hoa);
 
 	
 }
@@ -72,14 +72,16 @@ void sinhvien::enter() {
 
 void sinhvien::output() {	
 	cout << setw(6) << "Ma SV" << setw(8) << "Ten" << setw(-10) << "Gioi tinh" << setw(-12) << "Tuoi" << setw(-9) << "Diem toan" << setw(-10) << "Diem ly" << setw(-10) << "Diem hoa" << endl;
-	for (int i = 0; i < listsv.size(); i++) {
-	cout << setw(-14) << listsv[i].code << setw(-9) << listsv[i].name << setw(-12) << listsv[i].gender << setw(-14) << listsv[i].age << setw(-12) << listsv[i].maths << setw(-12) << listsv[i].physics << setw(-12) << listsv[i].chemis << endl; }
+	for (pair <string, sinhvien> x: listsv) {
+	cout << setw(-14) << (x.second).code << setw(-9) << (x.second).name << setw(-12) << (x.second).gender << setw(-14) << (x.second).age << setw(-12) << (x.second).maths << setw(-12) << (x.second).physics << setw(-12) << (x.second).chemis << endl; 
+	
+	}
 	
 }
 
 void sinhvien::edit(string masv) {
-	for (int i = 0; i < listsv.size(); i++) {
-		if (listsv[i].code == masv) {
+	for (pair <string, sinhvien> x:listsv) {
+		if ((x.second).code == masv) {
 			string ten, gioitinh;
 			double tuoi, toan, ly, hoa;
 			cout << "Nhap lai ten: " << endl;
@@ -94,7 +96,7 @@ void sinhvien::edit(string masv) {
 			cin >> ly;
 			cout << "Nhap lai diem hoa: " << endl;
 			cin >> hoa;
-			listsv[i].setsv(masv, ten, gioitinh, tuoi, toan, ly, hoa);
+			(x.second).setsv(masv, ten, gioitinh, tuoi, toan, ly, hoa);
 		}
 		else {
 			cout << "Ma SV ko co tren he thong." << endl;
@@ -103,9 +105,9 @@ void sinhvien::edit(string masv) {
 }
 
 void sinhvien::del(string masv) {
-	for (int i = 0; i < listsv.size(); i++) {
-		if (listsv[i].code == masv) {
-			listsv.erase(listsv.begin() + i);
+	for (pair <string, sinhvien> x:listsv) {
+		if ((x.second).code == masv) {
+			listsv.erase(x.first);
 		}
 		else {
 			cout << "Ma SV ko co tren he thong." << endl;
@@ -116,9 +118,9 @@ void sinhvien::del(string masv) {
 }
 
 void lop::seeallclass() {
-	for (int i = 0; i < listlop.size(); i++) {
+	for (pair <string, lop> x:listlop) {
 		cout << "Danh sach cac lop:" << endl;
-		cout << listlop[i].classname << endl;
+		cout << (x.second).classname << endl;
 	}
 }
 
@@ -127,16 +129,139 @@ void lop::addclass() {
 	string tenlop;
 	cout << "Nhap ten lop: " << endl;
 	getline(cin, tenlop);
-	if (check == listlop.end()) {
-		listlop.push_back(lop(tenlop));
+	setclass(tenlop);
+	
+	
+	
+	
+}
+
+void lop::addstudentclass(string tenlop) {
+	for (pair <string, lop> x:listlop) {
+		if ((x.second).classname == tenlop) {
+			string ten;
+			cout << "Hay them hoc sinh vao lop " << (x.second).classname << ": " << endl;
+			getline(cin, ten);
+			for (pair <string, sinhvien> y:listsv) {
+				if ((y.second).name == ten) {
+					((x.second).listsvlop).push_back(y.second);
+				}
+				else {
+					cout << "Khong co hoc sinh nay tren he thong." << endl;
+				}
+					
+			}
+		}
+		else {
+			cout << "Lop nay chua co tren he thong. Hay them lop vao de them hoc sinh." << endl;
+		}
+	}
+}
+
+void lop::studentinclass(string tenlop) {
+	cout << "Cac hoc sinh trong lop: " << tenlop << endl;
+	for (pair <string, lop> x:listlop) {
+		if ((x.second).classname == tenlop) {
+			for (int j = 0; j < ((x.second).listsvlop).size(); j++) {
+				cout << (x.second).listsvlop[j].name << endl;
+
+			}
+		}
+		else {
+			cout << "Khong co lop nay" << endl;
+		}
+		}
 	}
 	
-	
-}
-void lop::addstudentclass() {
-	
+void delstudentclass(string tenlop) {
+	for (pair <string, lop> x:listlop) {
+		if ((x.second).classname == tenlop) {
+			string ten;
+			cout << "Hay xoa hoc sinh trong lop " << (x.second).classname << ": " << endl;
+			for (int i = 0; i < ((x.second).listsvlop).size(); i++) {
+				if ((x.second).listsvlop.begin(), (x.second).listsvlop.end(), (x.second).listsvlop[i]) {
+					(x.second).listsvlop.erase(i);
+				}
+			};
+		}
+	}
 }
 
-
+	
+int main()
+{
+    int input;
+    string code;
+    sinhvien sv;
+    lop lp;
+    string classname;
+    while (true)
+    {
+        cout << "HE THONG QUAN LY SINH VIEN\n";
+        cout << "CHUC NANG : \n";
+        cout << "1. Them thong tin sinh vien." << endl;
+        cout << "2. Dua ra thong tin sinh vien" << endl;
+        cout << "3. Xoa thong tin sinh vien." << endl;
+        cout << "4. Chinh sua thong tin sinh vien." << endl;
+        cout << "5. Them lop hoc." << endl;
+        cout << "6. Xem danh sach lop hoc." << endl;
+        cout << "7. Them sinh vien vao lop." << endl;
+        cout << "8. Xem danh sach sinh vien trong lop." << endl;
+        cout << "9. Xoa sinh vien khoi lop hoc." << endl;
+        cout << "0. Thoat." << endl;
+        cout << "Hay chon chuc nang: " ; 
+        cin >> input;
+        if (input == 1) {
+        	sv.enter();
+        	listsv.insert(sv.name, sv);
+        	break;
+		}
+		if (input == 2) {
+			sv.output();
+			break;
+		}
+		if (input == 3) {
+			cout << "Nhap ma SV ban muon xoa: ";
+			getline(cin,code);
+			sv.del(code);
+			break;
+		}
+		if (input == 4) {
+			cout << "Nhap ma SV ban muon sua: ";
+			getline(cin,code);
+			sv.edit(code);
+			break;
+		}
+		if (input == 5) {
+			lp.addclass();
+			listlop.insert(lp.classname,lp);
+			break;
+		}
+		if (input == 6) {
+			lp.seeallclass();
+			break;
+		}
+		if (input == 7) {
+			cout << "Ten lop ma ban muon them hoc sinh: ";
+			lp.addstudentclass(classname);
+			break;
+		}
+		if (input == 8) {
+			cout << "Ten lop ma ban muon xem hoc sinh: ";
+			getline(cin, classname);
+			lp.studentinclass(classname);
+			break;
+		}
+		if (input == 9) {
+			cout << "Ten lop ma ban muon xoa hoc sinh: ";
+			getline(cin, classname);
+			lp.delstudentclass(classname);
+			break;
+		}
+		if (input == 0) {
+			return 0;
+		}
+   	 }
+}
 
 
