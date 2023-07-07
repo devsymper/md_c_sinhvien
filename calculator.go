427272f9767f2a4dbc08a81a0f0a0c04069b471b
ductlm <137411085+ductlm@users.noreply.github.com>
2023-07-07 17:00:51 +0700

    Add files via upload

---
+++ calculator.go
@@ -0,0 +1,21 @@
+package calculator
+
+func Add(A, B float32) (Sum float32) {
+	Sum = A + B
+	return
+}
+
+func Subtract(A, B float32) (Sub float32) {
+	Sub = A - B
+	return
+}
+
+func Multiply(A, B float32) (Mul float32) {
+	Mul = A * B
+	return
+}
+
+func Divide(A, B float32) (Div float32) {
+	Div = A / B
+	return
+}
